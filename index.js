5b25ad3f440a7f112cba2fc1de576c735e24a2f1
OktayCopurlu <oktaycopurlu1@gmail.com>
2024-12-29 07:12:05 +0100

    update TP and SL side

--- index.js
+++ index.js
@@ -23,26 +23,19 @@ app.use(express.json());
 // Function to parse incoming webhook messages
 function parseSignal(jsonSignal) {
   try {
-    const { symbol, price, signal, message } = jsonSignal;
-
+    const { symbol, price, signal } = jsonSignal;
+    let newSymbol = symbol;
     if (!signal) {
       console.log("No actionable signal found");
       return null;
     }
     // Remove .P suffix if it exists
-    if (symbol.endsWith(".P")) {
-      symbol = symbol.replace(".P", "");
+    if (symbol.includes(".P")) {
+      newSymbol = symbol.replace(".P", "").trim();
     }
 
-    const SL_TP = message?.includes("SL")
-      ? "SL"
-      : message?.includes("TP")
-      ? "TP"
-      : undefined;
-
     return {
-      action: SL_TP,
-      symbol,
+      symbol: newSymbol,
       signal,
       entry: parseFloat(price),
     };
@@ -120,15 +113,15 @@ async function placeOrder(signal) {
 
     if (openPositions.result) {
       const openPosition = openPositions.result.list[0];
-      const currentSide = openPosition.side;
+      const openPositionSide = openPosition.side;
 
       // If the current signal is the opposite of the open position, close the open position first
-      if (currentSide !== side) {
-        if (currentSide !== "") {
+      if (openPositionSide !== side) {
+        if (openPositionSide !== "") {
           const orderResponse = await bybitClient.submitOrder({
             category: "linear",
             symbol: signal.symbol,
-            side: currentSide === "Buy" ? "Sell" : "Buy",
+            side: openPositionSide === "Buy" ? "Sell" : "Buy",
             orderType: "Market",
             qty: openPosition.size,
             timeInForce: "GoodTillCancel",
@@ -138,39 +131,16 @@ async function placeOrder(signal) {
             return `Failed to close position: ${orderResponse.retMsg}`;
           }
 
-          const activeOrders = await bybitClient.getActiveOrders({
-            category: "linear",
-            symbol: signal.symbol,
-          });
-
-          if (
-            activeOrders.result &&
-            activeOrders.result.list &&
-            activeOrders.result.list.length > 0
-          ) {
-            for (const order of activeOrders.result.list) {
-              const cancelOrder = await bybitClient.cancelOrder({
-                category: "linear",
-                symbol: signal.symbol,
-                orderId: order.orderId,
-              });
-
-              if (instrumentDetails.retCode !== 0) {
-                return `Failed to cancelOrder : ${cancelOrder.retMsg}`;
-              }
-            }
-          } else {
-            console.log(`No conditional orders to cancel for ${signal.symbol}`);
-          }
+          return `Position closed: ${signal.symbol}`;
         } else {
           console.log(`No conditional orders to cancel for ${signal.symbol}`);
         }
       } else {
-        return `Open ${currentSide} position already exists for ${signal.symbol}`;
+        return `Open ${openPositionSide} position already exists for ${signal.symbol}`;
       }
     }
 
-    if (signal.action === undefined) {
+    if (side !== "Close") {
       // Place the new  order
       const response = await bybitClient.submitOrder({
         category: "linear",
@@ -189,46 +159,46 @@ async function placeOrder(signal) {
           `Limit Order placed: ${signal.symbol} ${side}, with ${totalMarginSize} USDT margin, ${targetLeverage}x leverage. Quantity: ${calculatedQuantity}, Price: ${limitPrice}`
         );
       }
-    }
 
-    // Pozisyon açıldıktan sonra %12.5'i için take profit ayarla
-    const takeProfitQuantity = (calculatedQuantity * 0.25).toFixed(
-      qtyPrecision
-    );
-    const takeProfitPrice =
-      side === "Buy"
-        ? (symbolPrice * 1.005).toFixed(4) // %12.5 yukarı fiyat
-        : (symbolPrice * 0.995).toFixed(4); // %12.5 aşağı fiyat
+      // Pozisyon açıldıktan sonra %12.5'i için take profit ayarla
+      const takeProfitQuantity = (calculatedQuantity * 0.25).toFixed(
+        qtyPrecision
+      );
+      const takeProfitPrice =
+        side === "Buy"
+          ? (symbolPrice * 1.005).toFixed(4) // %12.5 yukarı fiyat
+          : (symbolPrice * 0.995).toFixed(4); // %12.5 aşağı fiyat
 
-    const position = await bybitClient.getPositionInfo({
-      category: "linear",
-      symbol: signal.symbol,
-    });
-
-    if (position.retCode !== 0) {
-      return `Failed to close position: ${position.retMsg}`;
-    }
-
-    if (position.result.list[0].size > 0) {
-      const takeProfitResponse = await bybitClient.setTradingStop({
+      const position = await bybitClient.getPositionInfo({
         category: "linear",
         symbol: signal.symbol,
-        takeProfit: takeProfitPrice,
-        tpTriggerBy: "MarkPrice",
-        tpslMode: "Partial",
-        tpOrderType: "Limit",
-        tpSize: takeProfitQuantity,
-        tpLimitPrice: takeProfitPrice,
-        positionIdx: 0,
       });
 
-      if (takeProfitResponse.retCode !== 0) {
-        return `Take profit rejected: ${takeProfitResponse.retMsg}`;
-      } else {
-        return `Take profit order placed: ${signal.symbol} ${takeProfitQuantity} at ${takeProfitPrice}`;
+      if (position.retCode !== 0) {
+        return `Failed to close position: ${position.retMsg}`;
       }
-    } else {
-      return "No open position for the specified symbol.";
+
+      if (position.result.list[0].size > 0) {
+        const takeProfitResponse = await bybitClient.setTradingStop({
+          category: "linear",
+          symbol: signal.symbol,
+          takeProfit: takeProfitPrice,
+          tpTriggerBy: "MarkPrice",
+          tpslMode: "Partial",
+          tpOrderType: "Limit",
+          tpSize: takeProfitQuantity,
+          tpLimitPrice: takeProfitPrice,
+          positionIdx: 0,
+        });
+
+        if (takeProfitResponse.retCode !== 0) {
+          return `Take profit rejected: ${takeProfitResponse.retMsg}`;
+        } else {
+          return `Take profit order placed: ${signal.symbol} ${takeProfitQuantity} at ${takeProfitPrice}`;
+        }
+      } else {
+        return "No open position for the specified symbol.";
+      }
     }
   } catch (error) {
     return `An error occurred while placing the order: ${JSON.stringify(
